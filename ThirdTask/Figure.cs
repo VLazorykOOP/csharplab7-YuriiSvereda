579f49e033fd7c11545f88f595fdd6777e22a03f
YuriiSvereda <svereda.yurii@chnu.edu.ua>
2024-05-27 10:35:16 +0300

    change square logic to pentegon

--- ThirdTask/Figure.cs
+++ ThirdTask/Figure.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,93 +10,125 @@ using System.Windows;
 
 namespace ThirdTask
 {
-    public abstract class Figure
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Size { get; set; }
-        public Brush Color { get; set; }
 
-        public abstract void Draw(Canvas canvas);
-    }
-
-    public class Pentagon : Figure
+    public abstract class ShapeElement
     {
-        public override void Draw(Canvas canvas)
+        public string Text { get; }
+        public Color Color { get; }
+
+        protected ShapeElement(string text, Color color)
         {
-            Polygon pentagon = new Polygon
-            {
-                Stroke = Color,
-                Fill = Color,
-                StrokeThickness = 2,
-                Points = new PointCollection()
-                {
-                    new Point(X, Y),
-                    new Point(X + Size, Y),
-                    new Point(X + Size * 1.5, Y + Size),
-                    new Point(X + Size / 2, Y + Size * 1.5),
-                    new Point(X - Size / 2, Y + Size)
-                }
-            };
-            canvas.Children.Add(pentagon);
+            Text = text;
+            Color = color;
         }
-    }
 
-    public class Rhombus : Figure
-    {
-        public override void Draw(Canvas canvas)
+        public abstract void Draw(Canvas canvas, int x, int y);
+
+        protected void AddText(Canvas canvas, int x, int y, string text)
         {
-            Polygon rhombus = new Polygon
+            var textBlock = new TextBlock
             {
-                Stroke = Color,
-                Fill = Color,
-                StrokeThickness = 2,
-                Points = new PointCollection()
-                {
-                    new Point(X, Y),
-                    new Point(X + Size, Y - Size / 2),
-                    new Point(X + Size * 2, Y),
-                    new Point(X + Size, Y + Size / 2)
-                }
-            };
-            canvas.Children.Add(rhombus);
-        }
-    }
-
-    public class Triangle : Figure
-    {
-        public string Text { get; set; }
-
-        public override void Draw(Canvas canvas)
-        {
-            Polygon triangle = new Polygon
-            {
-                Stroke = Color,
-                Fill = Color,
-                StrokeThickness = 2,
-                Points = new PointCollection()
-                {
-                    new Point(X, Y),
-                    new Point(X + Size, Y),
-                    new Point(X + Size / 2, Y - Math.Sqrt(Size * Size - (Size / 2) * (Size / 2)))
-                }
-            };
-            canvas.Children.Add(triangle);
-
-            TextBlock textBlock = new TextBlock
-            {
-                Text = Text,
-                Foreground = Brushes.Black,
-                Background = Brushes.White,
-                FontSize = 12,
-                Width = Size,
-                TextAlignment = TextAlignment.Center
+                Text = text,
+                Foreground = new SolidColorBrush(Colors.Black),
+                Background = new SolidColorBrush(Colors.White)
             };
 
-            Canvas.SetLeft(textBlock, X);
-            Canvas.SetTop(textBlock, Y - Size / 2);
+            Canvas.SetLeft(textBlock, x - 10);
+            Canvas.SetTop(textBlock, y - 10);
+
             canvas.Children.Add(textBlock);
         }
     }
+
+
+    public class Rhombus : ShapeElement
+    {
+        public int Diagonal1 { get; }
+        public int Diagonal2 { get; }
+
+        public Rhombus(string text, Color color, int diagonal1, int diagonal2)
+            : base(text, color)
+        {
+            Diagonal1 = diagonal1;
+            Diagonal2 = diagonal2;
+        }
+
+        public override void Draw(Canvas canvas, int x, int y)
+        {
+            var polygon = new Polygon
+            {
+                Stroke = new SolidColorBrush(Color),
+                StrokeThickness = 2
+            };
+
+            polygon.Points.Add(new Point(x, y - Diagonal2 / 2));
+            polygon.Points.Add(new Point(x + Diagonal1 / 2, y));
+            polygon.Points.Add(new Point(x, y + Diagonal2 / 2));
+            polygon.Points.Add(new Point(x - Diagonal1 / 2, y));
+
+            canvas.Children.Add(polygon);
+            AddText(canvas, x, y, Text);
+        }
+    }
+
+
+    public class EquilateralTriangle : ShapeElement
+    {
+        public EquilateralTriangle(string text, Color color)
+            : base(text, color)
+        {
+        }
+
+        public override void Draw(Canvas canvas, int x, int y)
+        {
+            var polygon = new Polygon
+            {
+                Stroke = new SolidColorBrush(Color),
+                StrokeThickness = 2
+            };
+
+            double height = Math.Sqrt(3) / 2 * 50;
+            polygon.Points.Add(new Point(x, y - height / 2));
+            polygon.Points.Add(new Point(x - 25, y + height / 2));
+            polygon.Points.Add(new Point(x + 25, y + height / 2));
+
+            canvas.Children.Add(polygon);
+            AddText(canvas, x, y, Text);
+        }
+    }
+
+
+    public class Pentagon : ShapeElement
+    {
+        public int SideLength { get; } = 50;
+
+        public Pentagon(string text, Color color, int sideLength)
+            : base(text, color)
+        {
+            SideLength = sideLength;
+        }
+        public Pentagon(string text, Color color)
+            : base(text, color) { }
+
+        public override void Draw(Canvas canvas, int x, int y)
+        {
+            var polygon = new Polygon
+            {
+                Stroke = new SolidColorBrush(Color),
+                StrokeThickness = 2
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                double angle = (2 * Math.PI / 5) * i - Math.PI / 2; // Починаємо з верхньої вершини
+                double px = x + SideLength * Math.Cos(angle);
+                double py = y + SideLength * Math.Sin(angle);
+                polygon.Points.Add(new Point(px, py));
+            }
+
+            canvas.Children.Add(polygon);
+            AddText(canvas, x, y, Text);
+        }
+    }
+
 }
-*/
--- ThirdTask/MainWindow.xaml.cs
+++ ThirdTask/MainWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace ThirdTask
             }
             else if (SquareRadioButton.IsChecked == true)
             {
-                element = new Square(text, color);
+                element = new Pentagon(text, color);
             }
 
             if (element != null)
@@ -68,131 +68,6 @@ namespace ThirdTask
         }
     }
 
-    public abstract class ShapeElement
-    {
-        public string Text { get; }
-        public Color Color { get; }
 
-        protected ShapeElement(string text, Color color)
-        {
-            Text = text;
-            Color = color;
-        }
-
-        public abstract void Draw(Canvas canvas, int x, int y);
-
-        protected void AddText(Canvas canvas, int x, int y, string text)
-        {
-            var textBlock = new TextBlock
-            {
-                Text = text,
-                Foreground = new SolidColorBrush(Colors.Black),
-                Background = new SolidColorBrush(Colors.White)
-            };
-
-            Canvas.SetLeft(textBlock, x - 10);
-            Canvas.SetTop(textBlock, y - 10);
-
-            canvas.Children.Add(textBlock);
-        }
-    }
-
-
-    public class Rhombus : ShapeElement
-    {
-        public int Diagonal1 { get; }
-        public int Diagonal2 { get; }
-
-        public Rhombus(string text, Color color, int diagonal1, int diagonal2)
-            : base(text, color)
-        {
-            Diagonal1 = diagonal1;
-            Diagonal2 = diagonal2;
-        }
-
-        public override void Draw(Canvas canvas, int x, int y)
-        {
-            var polygon = new Polygon
-            {
-                Stroke = new SolidColorBrush(Color),
-                StrokeThickness = 2
-            };
-
-            polygon.Points.Add(new Point(x, y - Diagonal2 / 2));
-            polygon.Points.Add(new Point(x + Diagonal1 / 2, y));
-            polygon.Points.Add(new Point(x, y + Diagonal2 / 2));
-            polygon.Points.Add(new Point(x - Diagonal1 / 2, y));
-
-            canvas.Children.Add(polygon);
-            AddText(canvas, x, y, Text);
-        }
-    }
-
-
-    public class EquilateralTriangle : ShapeElement
-    {
-        public EquilateralTriangle(string text, Color color)
-            : base(text, color)
-        {
-        }
-
-        public override void Draw(Canvas canvas, int x, int y)
-        {
-            var polygon = new Polygon
-            {
-                Stroke = new SolidColorBrush(Color),
-                StrokeThickness = 2
-            };
-
-            double height = Math.Sqrt(3) / 2 * 50;
-            polygon.Points.Add(new Point(x, y - height / 2));
-            polygon.Points.Add(new Point(x - 25, y + height / 2));
-            polygon.Points.Add(new Point(x + 25, y + height / 2));
-
-            canvas.Children.Add(polygon);
-            AddText(canvas, x, y, Text);
-        }
-    }
-
-
-    public class Square : ShapeElement
-    {
-        public Square(string text, Color color)
-            : base(text, color)
-        {
-        }
-
-        public override void Draw(Canvas canvas, int x, int y)
-        {
-            var rect = new Rectangle
-            {
-                Width = 50,
-                Height = 50,
-                Stroke = new SolidColorBrush(Color),
-                StrokeThickness = 2
-            };
-
-            Canvas.SetLeft(rect, x - 25);
-            Canvas.SetTop(rect, y - 25);
-
-            canvas.Children.Add(rect);
-            AddText(canvas, x, y, Text);
-        }
-    }
-
-    /*private static void AddText(Canvas canvas, int x, int y, string text)
-    {
-        var textBlock = new TextBlock
-        {
-            Text = text,
-            Foreground = new SolidColorBrush(Colors.Black),
-            Background = new SolidColorBrush(Colors.White)
-        };
-
-        Canvas.SetLeft(textBlock, x - 10);
-        Canvas.SetTop(textBlock, y - 10);
-
-        canvas.Children.Add(textBlock);
-    }*/
 
 }
